88248c9601e0f2ff6882a8aa4573bfbf6197c4ad
Aadil2003 <143945971+Aadil2003@users.noreply.github.com>
2024-08-24 12:58:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,121 +1,13 @@
-// const circle = document.querySelector("#circle");
-// const box = document.querySelector("#box");
-// const score = document.querySelector("#score");
-// const btn = document.querySelector("#reset");
-// // const pro = document.querySelector("#reset");
-// const 
-
-// let scoree = 0;
-// let speed = 1000;
-// let moveInterval;
-
-
-// function move() {
-//     let maxX = box.clientWidth - circle.clientWidth;
-//     let maxY = box.clientHeight - circle.clientHeight;
-//     let randomX = Math.floor(Math.random() * maxX);
-//     let randomY = Math.floor(Math.random() * maxY);
-
-//     circle.style.left = `${randomX}px`;
-//     circle.style.top = `${randomY}px`;
-// }
-
-
-// var normalmode= function update() {
-//     scoree++;
-//     speed = Math.max(300, speed - 50);
-//     score.textContent = scoree;
-//     if (!moveInterval) {
-    
-//       move();
-//       moveInterval = setInterval(move, speed);
-//   }
-
-
-//     if (moveInterval) {
-//         clearInterval(moveInterval);
-//     }
-
-
-//     move();
-
-    
-//     moveInterval = setInterval(move, speed);
-// }
-
-// var promode=function update() {
-//   scoree++;
-//   speed = Math.max(300, speed - 200);
-//   score.textContent = scoree;
-//   if (!moveInterval) {
-  
-//     move();
-//     moveInterval = setInterval(move, speed);
-// }
-
-
-//   if (moveInterval) {
-//       clearInterval(moveInterval);
-//   }
-
-
-//   move();
-
-  
-//   moveInterval = setInterval(move, speed);
-// }
-
-
-// circle.addEventListener("click" , function() {
-   
-//   runModeFunction();
-    
-
-//     const clickSound = document.getElementById('clickSound');
-//     clickSound.play();
-//     clickSound.currentTime = 0;
-// });
-
-// circle.addEventListener("dblclick" , function() {
-   
-//   runModeFunction();
-  
-
-//   const clickSound = document.getElementById('clickSound');
-//   clickSound.play();
-//   clickSound.currentTime = 0;
-// });
-// btn.addEventListener("click",()=>{
-//   clearInterval(moveInterval);
-//   score.textContent = 0
-//   scoree=0
-//    circle.style.top="420px";
-//    circle.style.left= "5px";
-// });
-// // pro.addEventListener("click",()=>{
-  
-// // })
-// function runModeFunction() {
-//   const selectedRadio = document.querySelector('input[name="mode"]:checked');
-//   if (selectedRadio) {
-//       const value = selectedRadio.value;
-//       if (value === "pro") {
-//           promode();
-//       } else if (value === "Normal-Mode") {
-//         normalmode();
-//       }
-//   } else {
-//       console.log("No mode selected.");
-//   }
-// }
 const circle = document.querySelector("#circle");
 const box = document.querySelector("#box");
 const score = document.querySelector("#score");
 const btn = document.querySelector("#reset");
+const clickSound = document.getElementById('clickSound');
 
 let scoree = 0;
 let speed = 1000;
 let moveInterval;
+let debounce = false;
 
 function move() {
     let maxX = box.clientWidth - circle.clientWidth;
@@ -141,33 +33,11 @@ function proMode() {
     scoree++;
     speed = Math.max(300, speed - 200);
     score.textContent = scoree;
-
+    
     clearInterval(moveInterval);
     move();
     moveInterval = setInterval(move, speed);
 }
-
-circle.addEventListener("click", function() {
-    runModeFunction();
-    const clickSound = document.getElementById('clickSound');
-    clickSound.play();
-    clickSound.currentTime = 0;
-});
-
-circle.addEventListener("dblclick", function() {
-    runModeFunction();
-    const clickSound = document.getElementById('clickSound');
-    clickSound.play();
-    clickSound.currentTime = 0;
-});
-
-btn.addEventListener("click", () => {
-    clearInterval(moveInterval);
-    score.textContent = 0;
-    scoree = 0;
-    circle.style.top = "420px";
-    circle.style.left = "5px";
-});
 
 function runModeFunction() {
     const selectedRadio = document.querySelector('input[name="mode"]:checked');
@@ -179,6 +49,42 @@ function runModeFunction() {
             normalMode();
         }
     } else {
-       window.alert("Choose a mode to continue")
+        window.alert("Choose a mode to continue");
+    }
+}
+
+
+circle.addEventListener("click", function() {
+    if (!debounce) {
+        debounce = true;
+        runModeFunction();
+        playClickSound();
+        debounce = false;
+    }
+});
+
+circle.addEventListener("dblclick", function() {
+    if (!debounce) {
+        debounce = true;
+        runModeFunction();
+        playClickSound();
+        debounce = false;
+    }
+});
+
+btn.addEventListener("click", () => {
+    clearInterval(moveInterval);
+    score.textContent = 0;
+    scoree = 0;
+    circle.style.top = "420px";
+    circle.style.left = "5px";
+});
+
+
+function playClickSound() {
+    if (clickSound) {
+        clickSound.pause();
+        clickSound.currentTime = 0;
+        clickSound.play();
     }
 }
